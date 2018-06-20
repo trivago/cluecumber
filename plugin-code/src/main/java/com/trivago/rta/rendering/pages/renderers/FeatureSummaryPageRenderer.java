c66132300c42234e679ddfd255b490c07c07a671
Benjamin Bischoff <benjamin.bischoff@trivago.com>
2018-06-20 09:22:18 +0200

    added feature summary graph

--- plugin-code/src/main/java/com/trivago/rta/rendering/pages/renderers/FeatureSummaryPageRenderer.java
+++ plugin-code/src/main/java/com/trivago/rta/rendering/pages/renderers/FeatureSummaryPageRenderer.java
@@ -16,18 +16,85 @@
 
 package com.trivago.rta.rendering.pages.renderers;
 
+import be.ceau.chart.BarChart;
+import be.ceau.chart.data.BarData;
+import be.ceau.chart.dataset.BarDataset;
+import be.ceau.chart.options.BarOptions;
+import be.ceau.chart.options.scales.BarScale;
+import be.ceau.chart.options.scales.XAxis;
+import be.ceau.chart.options.scales.YAxis;
+import be.ceau.chart.options.ticks.LinearTicks;
+import com.trivago.rta.constants.ChartColor;
+import com.trivago.rta.constants.Status;
 import com.trivago.rta.exceptions.CluecumberPluginException;
+import com.trivago.rta.rendering.pages.pojos.Feature;
+import com.trivago.rta.rendering.pages.pojos.ResultCount;
 import com.trivago.rta.rendering.pages.pojos.pagecollections.FeatureSummaryPageCollection;
 import freemarker.template.Template;
 
 import javax.inject.Singleton;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Singleton
 public class FeatureSummaryPageRenderer extends PageRenderer {
     public String getRenderedContent(
             final FeatureSummaryPageCollection featureSummaryPageCollection, final Template template)
             throws CluecumberPluginException {
+
+        addChartJsonToReportDetails(featureSummaryPageCollection);
         return processedContent(template, featureSummaryPageCollection);
+    }
+
+    private void addChartJsonToReportDetails(final FeatureSummaryPageCollection featureSummaryPageCollection) {
+        List<BarDataset> barDatasets = new ArrayList<>();
+
+        List<BigDecimal> passed = new ArrayList<>();
+        List<BigDecimal> failed = new ArrayList<>();
+        List<BigDecimal> skipped = new ArrayList<>();
+
+        int maxY = 0;
+
+        for (Map.Entry<Feature, ResultCount> entry : featureSummaryPageCollection.getFeatureResultCounts().entrySet()) {
+            passed.add(BigDecimal.valueOf(entry.getValue().getPassed()));
+            failed.add(BigDecimal.valueOf(entry.getValue().getFailed()));
+            skipped.add(BigDecimal.valueOf(entry.getValue().getSkipped()));
+            maxY = entry.getValue().getTotal();
+        }
+
+        barDatasets.add(new BarDataset().setLabel("passed").setData(passed)
+                .setBackgroundColor(ChartColor.getChartColorByStatus(Status.PASSED)));
+        barDatasets.add(new BarDataset().setLabel("failed")
+                .setData(failed).setBackgroundColor(ChartColor.getChartColorByStatus(Status.FAILED)));
+        barDatasets.add(new BarDataset().setLabel("skipped").setData(skipped)
+                .setBackgroundColor(ChartColor.getChartColorByStatus(Status.SKIPPED)));
+
+        BarData barData = new BarData();
+        barData.setDatasets(barDatasets);
+
+        List<String> keys = new ArrayList<>();
+        for (Feature feature : featureSummaryPageCollection.getFeatureResultCounts().keySet()) {
+            keys.add(feature.getName());
+        }
+        barData.setLabels(keys);
+
+        BarOptions barOptions = new BarOptions();
+        BarScale barScale = new BarScale();
+
+        List<XAxis<LinearTicks>> xAxisList = new ArrayList<>();
+        xAxisList.add(new XAxis<LinearTicks>().setStacked(true).setTicks(new LinearTicks().setMin(0)));
+        barScale.setxAxes(xAxisList);
+
+        List<YAxis<LinearTicks>> yAxisList = new ArrayList<>();
+        yAxisList.add(new YAxis<LinearTicks>().setStacked(true).setTicks(new LinearTicks().setMin(0).setStepSize(maxY)));
+        barScale.setyAxes(yAxisList);
+
+        barOptions.setScales(barScale);
+
+        String chartJson = new BarChart(barData, barOptions).toJson();
+        featureSummaryPageCollection.getReportDetails().setChartJson(chartJson);
     }
 }
 
