99299702374378715de9c3464080bf681e9d7bc7
Benjamin Bischoff <benjamin.bischoff@trivago.com>
2018-06-20 10:14:01 +0200

    added html encode test

--- plugin-code/src/test/java/com/trivago/rta/rendering/RenderingUtilsTest.java
+++ plugin-code/src/test/java/com/trivago/rta/rendering/RenderingUtilsTest.java
@@ -7,20 +7,27 @@ import static org.hamcrest.core.Is.is;
 
 public class RenderingUtilsTest {
     @Test
-    public void convertMicrosecondsToTimeStringTest(){
+    public void convertMicrosecondsToTimeStringTest() {
         String timeString = RenderingUtils.convertMicrosecondsToTimeString(1234567890000l);
         assertThat(timeString, is("20m 34s 567ms"));
     }
 
     @Test
-    public void convertMicrosecondsToMillisecondsTest(){
+    public void convertMicrosecondsToMillisecondsTest() {
         long milliseconds = RenderingUtils.convertMicrosecondsToMilliseconds(1234567890000l);
         assertThat(milliseconds, is(1234567L));
     }
 
     @Test
-    public void getPluginVersionTest(){
+    public void getPluginVersionTest() {
         String version = RenderingUtils.getPluginVersion();
         assertThat(version, is("unknown"));
+    }
+
+    @Test
+    public void escapeHTMLTest() {
+        String escapedHTML = RenderingUtils.escapeHTML("<body>This is a test öäüÖÄÜß</body>");
+        assertThat(escapedHTML, is("&#60;body&#62;This is a test &#246;&#228;&#252;&#214;&#196;&#220;&#223;&#60;/body&#62;"));
+
     }
 }
