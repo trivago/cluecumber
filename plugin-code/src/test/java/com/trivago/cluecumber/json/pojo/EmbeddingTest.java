a8fbaf5bcb8016af070ded9200ae91c687137dc4
Rameshwar Shiral <12494447+Rameshwar244@users.noreply.github.com>
2018-10-24 18:15:01 +0530

    Update EmbeddingTest.java

--- plugin-code/src/test/java/com/trivago/cluecumber/json/pojo/EmbeddingTest.java
+++ plugin-code/src/test/java/com/trivago/cluecumber/json/pojo/EmbeddingTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.codehaus.plexus.util.Base64;
+
 public class EmbeddingTest {
     private Embedding embedding;
 
