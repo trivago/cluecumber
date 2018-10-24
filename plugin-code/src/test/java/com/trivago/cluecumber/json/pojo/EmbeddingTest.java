78d073ba1e5848ee587b17cf84e70524a6d1f572
Rameshwar Shiral <12494447+Rameshwar244@users.noreply.github.com>
2018-10-24 15:00:10 +0530

    Added junit tests

--- plugin-code/src/test/java/com/trivago/cluecumber/json/pojo/EmbeddingTest.java
+++ plugin-code/src/test/java/com/trivago/cluecumber/json/pojo/EmbeddingTest.java
@@ -55,4 +55,51 @@ public class EmbeddingTest {
         embedding.setMimeType("text/plain");
         assertThat(embedding.isPlainText(), is(true));
     }
+    
+    @Test
+    public void getDecodedDataTest() {
+		String originalInput = "This is getDecodeData() Test !!!";
+		String encodeString = new String(Base64.encodeBase64(originalInput.getBytes()));
+		embedding.setData(encodeString);
+		assertThat(embedding.getDecodedData(),is("This is getDecodeData() Test !!!"));
+    }
+    
+    @Test
+    public void getDecodedDataForXMLTest() {
+		String originalInput = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+		String encodeString = new String(Base64.encodeBase64(originalInput.getBytes()));
+		embedding.setData(encodeString);
+        embedding.setMimeType("text/xml");
+		assertThat(embedding.getDecodedData(),is("&lt;?xml version=\"1.0\" encoding=\"UTF-8\"?&gt;&lt;note&gt;&lt;to&gt;Tove&lt;/to&gt;&lt;from&gt;Jani&lt;/from&gt;&lt;heading&gt;Reminder&lt;/heading&gt;&lt;body&gt;Don't forget me this weekend!&lt;/body&gt;&lt;/note&gt;"));        
+    }
+    
+    @Test
+    public void getFileEndingTest() {
+        embedding.setMimeType("image/png");
+        assertThat(embedding.getFileEnding(), is("png"));
+        embedding.setMimeType("image/gif");
+        assertThat(embedding.getFileEnding(), is("gif"));
+        embedding.setMimeType("image/bmp");
+        assertThat(embedding.getFileEnding(), is("bmp"));
+        embedding.setMimeType("image/jpeg");
+        assertThat(embedding.getFileEnding(), is("jpeg"));
+        embedding.setMimeType("text/html");
+        assertThat(embedding.getFileEnding(), is("html"));
+        embedding.setMimeType("text/xml");
+        assertThat(embedding.getFileEnding(), is("xml"));
+        embedding.setMimeType("application/json");
+        assertThat(embedding.getFileEnding(), is("json"));
+        embedding.setMimeType("application/xml");
+        assertThat(embedding.getFileEnding(), is("xml"));
+        embedding.setMimeType("image/svg");
+        assertThat(embedding.getFileEnding(), is("svg"));
+        embedding.setMimeType("image/svg+xml");
+        assertThat(embedding.getFileEnding(), is("svg"));
+        embedding.setMimeType("text/plain");        
+        assertThat(embedding.getFileEnding(), is("txt"));
+        embedding.setMimeType("application/pdf");
+        assertThat(embedding.getFileEnding(), is("pdf"));
+        embedding.setMimeType("unknown/unknown");
+        assertThat(embedding.getFileEnding(), is("unknown"));
+    }        
 }
