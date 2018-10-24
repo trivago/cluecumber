2a4562f5e20caeb411e84109382f5735c5aaa450
Rameshwar Shiral <12494447+Rameshwar244@users.noreply.github.com>
2018-10-24 18:13:06 +0530

    Update ElementTest.java

--- plugin-code/src/test/java/com/trivago/cluecumber/json/pojo/ElementTest.java
+++ plugin-code/src/test/java/com/trivago/cluecumber/json/pojo/ElementTest.java
@@ -1,107 +1,339 @@
 package com.trivago.cluecumber.json.pojo;
 
+import com.trivago.cluecumber.constants.Status;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.codehaus.plexus.util.Base64;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
-public class EmbeddingTest {
-    private Embedding embedding;
+public class ElementTest {
+    private Element element;
 
     @Before
     public void setup() {
-        embedding = new Embedding();
+        element = new Element();
     }
 
     @Test
-    public void isImagePngTest() {
-        embedding.setMimeType("image/png");
-        assertThat(embedding.isImage(), is(true));
+    public void getSkippedStatusInEmptyElementsTest() {
+        Status status = element.getStatus();
+        assertThat(status, is(Status.SKIPPED));
+        assertThat(element.isSkipped(), is(true));
     }
 
     @Test
-    public void isImageJpgTest() {
-        embedding.setMimeType("image/jpeg");
-        assertThat(embedding.isImage(), is(true));
+    public void getPassedStatusTest() {
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.PASSED));
+        assertThat(element.isPassed(), is(true));
     }
 
     @Test
-    public void isImageGifTest() {
-        embedding.setMimeType("image/gif");
-        assertThat(embedding.isImage(), is(true));
+    public void passedStatusOnPassedAndSkippedStepsTest() {
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+
+        step = new Step();
+        result = new Result();
+        result.setStatus("skipped");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.PASSED));
     }
 
     @Test
-    public void isImageSvgXmlTest() {
-        embedding.setMimeType("image/svg+xml");
-        assertThat(embedding.isImage(), is(true));
+    public void failedStatusOnFailedBeforeHookTest() {
+        List<com.trivago.cluecumber.json.pojo.ResultMatch> before = new ArrayList<>();
+        com.trivago.cluecumber.json.pojo.ResultMatch beforeHook = new com.trivago.cluecumber.json.pojo.ResultMatch();
+        Result beforeHookResult = new Result();
+        beforeHookResult.setStatus("failed");
+        beforeHook.setResult(beforeHookResult);
+        before.add(beforeHook);
+        element.setBefore(before);
+
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+
+        step = new Step();
+        result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.FAILED));
     }
 
     @Test
-    public void isImageSvgTest() {
-        embedding.setMimeType("image/svg");
-        assertThat(embedding.isImage(), is(true));
+    public void failedStatusOnFailedAfterHookTest() {
+        List<com.trivago.cluecumber.json.pojo.ResultMatch> after = new ArrayList<>();
+        com.trivago.cluecumber.json.pojo.ResultMatch afterHook = new com.trivago.cluecumber.json.pojo.ResultMatch();
+        Result afterHookResult = new Result();
+        afterHookResult.setStatus("failed");
+        afterHook.setResult(afterHookResult);
+        after.add(afterHook);
+        element.setAfter(after);
+
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+
+        step = new Step();
+        result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.FAILED));
     }
 
     @Test
-    public void isImageInvalidTest() {
-        embedding.setMimeType("no/image");
-        assertThat(embedding.isImage(), is(false));
+    public void failedStatusOnFailedAfterStepHookTest() {
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+
+        List<com.trivago.cluecumber.json.pojo.ResultMatch> after = new ArrayList<>();
+        com.trivago.cluecumber.json.pojo.ResultMatch afterStepHook = new com.trivago.cluecumber.json.pojo.ResultMatch();
+        Result afterStepHookResult = new Result();
+        afterStepHookResult.setStatus("failed");
+        afterStepHook.setResult(afterStepHookResult);
+        after.add(afterStepHook);
+        step.setAfter(after);
+
+        steps.add(step);
+
+        step = new Step();
+        result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.FAILED));
     }
 
     @Test
-    public void isPlainTextTest() {
-        embedding.setMimeType("text/plain");
-        assertThat(embedding.isPlainText(), is(true));
+    public void failedStatusOnFailedBeforeStepHookTest() {
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+
+        List<com.trivago.cluecumber.json.pojo.ResultMatch> before = new ArrayList<>();
+        com.trivago.cluecumber.json.pojo.ResultMatch beforeStepHook = new com.trivago.cluecumber.json.pojo.ResultMatch();
+        Result beforeStepHookResult = new Result();
+        beforeStepHookResult.setStatus("failed");
+        step.setResult(beforeStepHookResult);
+        before.add(beforeStepHook);
+        step.setBefore(before);
+
+        steps.add(step);
+
+        step = new Step();
+        result = new Result();
+        result.setStatus("passed");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.FAILED));
     }
-    
+
     @Test
-    public void getDecodedDataTest() {
-		String originalInput = "This is getDecodeData() Test !!!";
-		String encodeString = new String(Base64.encodeBase64(originalInput.getBytes()));
-		embedding.setData(encodeString);
-		assertThat(embedding.getDecodedData(),is("This is getDecodeData() Test !!!"));
+    public void getFailedStatusTest() {
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("failed");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.FAILED));
+        assertThat(element.isFailed(), is(true));
     }
-    
+
     @Test
-    public void getDecodedDataForXMLTest() {
-		String originalInput = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
-		String encodeString = new String(Base64.encodeBase64(originalInput.getBytes()));
-		embedding.setData(encodeString);
-        embedding.setMimeType("text/xml");
-		assertThat(embedding.getDecodedData(),is("&lt;?xml version=\"1.0\" encoding=\"UTF-8\"?&gt;&lt;note&gt;&lt;to&gt;Tove&lt;/to&gt;&lt;from&gt;Jani&lt;/from&gt;&lt;heading&gt;Reminder&lt;/heading&gt;&lt;body&gt;Don't forget me this weekend!&lt;/body&gt;&lt;/note&gt;"));        
+    public void getUndefinedStatusTest() {
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result result = new Result();
+        result.setStatus("undefined");
+        step.setResult(result);
+        steps.add(step);
+        element.setSteps(steps);
+
+        Status status = element.getStatus();
+        assertThat(status, is(Status.SKIPPED));
+        assertThat(element.isSkipped(), is(true));
     }
-    
+
     @Test
-    public void getFileEndingTest() {
-        embedding.setMimeType("image/png");
-        assertThat(embedding.getFileEnding(), is("png"));
-        embedding.setMimeType("image/gif");
-        assertThat(embedding.getFileEnding(), is("gif"));
-        embedding.setMimeType("image/bmp");
-        assertThat(embedding.getFileEnding(), is("bmp"));
-        embedding.setMimeType("image/jpeg");
-        assertThat(embedding.getFileEnding(), is("jpeg"));
-        embedding.setMimeType("text/html");
-        assertThat(embedding.getFileEnding(), is("html"));
-        embedding.setMimeType("text/xml");
-        assertThat(embedding.getFileEnding(), is("xml"));
-        embedding.setMimeType("application/json");
-        assertThat(embedding.getFileEnding(), is("json"));
-        embedding.setMimeType("application/xml");
-        assertThat(embedding.getFileEnding(), is("xml"));
-        embedding.setMimeType("image/svg");
-        assertThat(embedding.getFileEnding(), is("svg"));
-        embedding.setMimeType("image/svg+xml");
-        assertThat(embedding.getFileEnding(), is("svg"));
-        embedding.setMimeType("text/plain");        
-        assertThat(embedding.getFileEnding(), is("txt"));
-        embedding.setMimeType("application/pdf");
-        assertThat(embedding.getFileEnding(), is("pdf"));
-        embedding.setMimeType("unknown/unknown");
-        assertThat(embedding.getFileEnding(), is("unknown"));
-    }        
+    public void totalDurationTest() {
+        List<com.trivago.cluecumber.json.pojo.ResultMatch> beforeSteps = new ArrayList<>();
+        com.trivago.cluecumber.json.pojo.ResultMatch before = new com.trivago.cluecumber.json.pojo.ResultMatch();
+        Result beforeResult = new Result();
+        beforeResult.setDuration(1000000);
+        before.setResult(beforeResult);
+        beforeSteps.add(before);
+        element.setBefore(beforeSteps);
+
+        List<Step> steps = new ArrayList<>();
+        Step step = new Step();
+        Result stepResult = new Result();
+        stepResult.setDuration(9991000003L);
+        step.setResult(stepResult);
+        steps.add(step);
+
+        Step step2 = new Step();
+        Result stepResult2 = new Result();
+        stepResult2.setDuration(123667782L);
+        step2.setResult(stepResult2);
+        steps.add(step2);
+
+        element.setSteps(steps);
+
+        List<com.trivago.cluecumber.json.pojo.ResultMatch> afterSteps = new ArrayList<>();
+        com.trivago.cluecumber.json.pojo.ResultMatch after = new com.trivago.cluecumber.json.pojo.ResultMatch();
+        Result afterResult = new Result();
+        afterResult.setDuration(2000000);
+        after.setResult(afterResult);
+        afterSteps.add(after);
+        element.setAfter(afterSteps);
+
+        assertThat(element.getTotalDuration(), is(10117667785L));
+        assertThat(element.returnTotalDurationString(), is("0m 10s 117ms"));
+    }
+
+    @Test
+    public void stepSummaryTest() {
+        List<Step> steps = new ArrayList<>();
+
+        Step step1 = new Step();
+        Result result1 = new Result();
+        result1.setStatus("passed");
+        step1.setResult(result1);
+        steps.add(step1);
+        steps.add(step1);
+        steps.add(step1);
+
+        Step step2 = new Step();
+        Result result2 = new Result();
+        result2.setStatus("skipped");
+        step2.setResult(result2);
+        steps.add(step2);
+
+        Step step3 = new Step();
+        Result result3 = new Result();
+        result3.setStatus("pending");
+        step3.setResult(result3);
+        steps.add(step3);
+
+        Step step4 = new Step();
+        Result result4 = new Result();
+        result4.setStatus("failed");
+        step4.setResult(result4);
+        steps.add(step4);
+
+        element.setSteps(steps);
+
+        assertThat(element.getTotalNumberOfSteps(), is(6));
+        assertThat(element.getTotalNumberOfPassedSteps(), is(3));
+        assertThat(element.getTotalNumberOfFailedSteps(), is(1));
+        assertThat(element.getTotalNumberOfSkippedSteps(), is(2));
+    }
+
+    @Test
+    public void hasDocStringsTest(){
+        assertThat(element.hasDocStrings(), is(false));
+
+        List<Step> steps = new ArrayList<>();
+
+        Step step1 = new Step();
+        steps.add(step1);
+
+        Step step2 = new Step();
+        step2.setDocString(new DocString());
+        steps.add(step2);
+
+        element.setSteps(steps);
+
+        assertThat(element.hasDocStrings(), is(true));
+    }
+
+    @Test
+    public void hasStepHooksBeforeTest(){
+        assertThat(element.hasStepHooks(), is(false));
+
+        List<Step> steps = new ArrayList<>();
+
+        Step step1 = new Step();
+        steps.add(step1);
+
+        Step step2 = new Step();
+        List<ResultMatch> beforeStepHooks = new ArrayList<>();
+        beforeStepHooks.add(new ResultMatch());
+        step2.setBefore(beforeStepHooks);
+        steps.add(step2);
+
+        element.setSteps(steps);
+
+        assertThat(element.hasStepHooks(), is(true));
+    }
+
+    @Test
+    public void hasStepHooksAfterTest(){
+        assertThat(element.hasStepHooks(), is(false));
+
+        List<Step> steps = new ArrayList<>();
+
+        Step step1 = new Step();
+        steps.add(step1);
+
+        Step step2 = new Step();
+        List<ResultMatch> afterStepHooks = new ArrayList<>();
+        afterStepHooks.add(new ResultMatch());
+        step2.setAfter(afterStepHooks);
+        steps.add(step2);
+
+        element.setSteps(steps);
+
+        assertThat(element.hasStepHooks(), is(true));
+    }
 }
