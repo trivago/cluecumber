f716fc1c3dff3c23605d8f42f3c6a10998259744
Alberto Scotto <scotto.alberto.86@gmail.com>
2020-05-19 21:55:21 +0200

    Fully working example
    Set up the maven failsafe plugin, and add a test class.

---
+++ example-project/src/test/java/com/example/RunCucumberIT.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"json:target/cucumber/cucumber.json"})
+public class RunCucumberIT {}
