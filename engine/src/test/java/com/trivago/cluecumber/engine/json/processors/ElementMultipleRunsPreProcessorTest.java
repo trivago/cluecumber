40199519c5e9085d53fb71beb1d33b38b1442bf6
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2024-09-10 10:27:48 +0200

    tdd tests

--- engine/src/test/java/com/trivago/cluecumber/engine/json/processors/ElementMultipleRunsPreProcessorTest.java
+++ engine/src/test/java/com/trivago/cluecumber/engine/json/processors/ElementMultipleRunsPreProcessorTest.java
@@ -1,7 +1,10 @@
 package com.trivago.cluecumber.engine.json.processors;
 
+import com.trivago.cluecumber.engine.json.pojo.DocString;
 import com.trivago.cluecumber.engine.json.pojo.Element;
 import com.trivago.cluecumber.engine.json.pojo.Report;
+import com.trivago.cluecumber.engine.json.pojo.Row;
+import com.trivago.cluecumber.engine.json.pojo.Step;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +19,12 @@ public class ElementMultipleRunsPreProcessorTest {
     private ElementMultipleRunsPreProcessor elementMultipleRunsPreProcessor;
 
     @BeforeEach
-public void setup() {
+    public void setup() {
         elementMultipleRunsPreProcessor = new ElementMultipleRunsPreProcessor();
     }
 
     @Test
-    public void addMultipleRunsInformationToScenariosTest() {
+    public void addMultipleRunsInformationToGroupableScenariosTest() {
         List<Report> reports = new ArrayList<>();
         Report report = new Report();
         List<Element> elements = new ArrayList<>();
@@ -70,5 +73,151 @@ public void setup() {
         assertTrue(e.get(1).isMultiRunParent());
         assertFalse(e.get(1).isMultiRunChild());
         assertEquals(e.get(1).getMultiRunChildren().size(), 1);
+    }
+
+    @Test
+    public void addMultipleRunsInformationToSameScenariosWithDifferentKeywords() {
+        List<Report> reports = new ArrayList<>();
+        Report report = new Report();
+        List<Element> elements = new ArrayList<>();
+
+        Step step = new Step();
+        step.setKeyword("Given");
+        Element element = createNewElement(List.of(step));
+        elements.add(element);
+
+        step = new Step();
+        step.setKeyword("And");
+        element = createNewElement(List.of(step));
+        elements.add(element);
+
+        report.setElements(elements);
+        reports.add(report);
+
+        elementMultipleRunsPreProcessor.addMultipleRunsInformationToScenarios(reports);
+
+        List<Element> newElements = reports.get(0).getElements();
+        assertEquals(newElements.size(), 2);
+    }
+
+    @Test
+    public void addMultipleRunsInformationToSameScenariosWithDifferentParameterValues() {
+        List<Report> reports = new ArrayList<>();
+        Report report = new Report();
+        List<Element> elements = new ArrayList<>();
+
+        Step step = new Step();
+        step.setName("Given I have 4 apples");
+        Element element = createNewElement(List.of(step));
+        elements.add(element);
+
+        step = new Step();
+        step.setName("Given I have 5 apples");
+        element = createNewElement(List.of(step));
+        elements.add(element);
+
+        report.setElements(elements);
+        reports.add(report);
+
+        elementMultipleRunsPreProcessor.addMultipleRunsInformationToScenarios(reports);
+
+        List<Element> newElements = reports.get(0).getElements();
+        assertEquals(newElements.size(), 2);
+    }
+
+    @Test
+    public void addMultipleRunsInformationToSameScenariosWithDifferentDocStrings() {
+        List<Report> reports = new ArrayList<>();
+        Report report = new Report();
+        List<Element> elements = new ArrayList<>();
+
+        Step step = new Step();
+        DocString docString = new DocString();
+        docString.setValue("abc");
+        step.setDocString(docString);
+        Element element = createNewElement(List.of(step));
+        elements.add(element);
+
+        step = new Step();
+        docString = new DocString();
+        docString.setValue("def");
+        step.setDocString(docString);
+        element = createNewElement(List.of(step));
+        elements.add(element);
+
+        report.setElements(elements);
+        reports.add(report);
+
+        elementMultipleRunsPreProcessor.addMultipleRunsInformationToScenarios(reports);
+
+        List<Element> newElements = reports.get(0).getElements();
+        assertEquals(newElements.size(), 2);
+    }
+
+    @Test
+    public void addMultipleRunsInformationToSameScenariosWithDifferentOutputs() {
+        List<Report> reports = new ArrayList<>();
+        Report report = new Report();
+        List<Element> elements = new ArrayList<>();
+
+        Step step = new Step();
+        step.setOutput(List.of("abc"));
+        Element element = createNewElement(List.of(step));
+        elements.add(element);
+
+        step = new Step();
+        step.setOutput(List.of("def"));
+        element = createNewElement(List.of(step));
+        elements.add(element);
+
+        report.setElements(elements);
+        reports.add(report);
+
+        elementMultipleRunsPreProcessor.addMultipleRunsInformationToScenarios(reports);
+
+        List<Element> newElements = reports.get(0).getElements();
+        assertEquals(newElements.size(), 2);
+    }
+
+    @Test
+    public void addMultipleRunsInformationToSameScenariosWithDifferentDataTableRows() {
+        List<Report> reports = new ArrayList<>();
+        Report report = new Report();
+        List<Element> elements = new ArrayList<>();
+
+        Step step = new Step();
+        List<Row> rows = new ArrayList<>();
+        Row row = new Row();
+        row.setCells(List.of("abc"));
+        step.setRows(rows);
+        Element element = createNewElement(List.of(step));
+        elements.add(element);
+
+        step = new Step();
+        rows = new ArrayList<>();
+        row = new Row();
+        row.setCells(List.of("def"));
+        step.setRows(rows);
+        element = createNewElement(List.of(step));
+        elements.add(element);
+
+        report.setElements(elements);
+        reports.add(report);
+
+        elementMultipleRunsPreProcessor.addMultipleRunsInformationToScenarios(reports);
+
+        List<Element> newElements = reports.get(0).getElements();
+        assertEquals(newElements.size(), 2);
+    }
+
+    private Element createNewElement(final List<Step> steps) {
+        Element element = new Element();
+        element.setName("Scenario 1");
+        element.setType("scenario");
+        element.setId("feature1-scenario1");
+        element.setSteps(steps);
+        element.setLine(3);
+        element.setStartTimestamp("2019-04-11T08:00:21.123Z");
+        return element;
     }
 }
